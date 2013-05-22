5c88d3355c475d4ece447d189343942b7e056bae
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-05-23 02:03:21 +0400

    .

--- nh3test/nh3test/Program.cs
+++ nh3test/nh3test/Program.cs
@@ -1,4 +1,8 @@
-﻿/*
+﻿using System.Reflection;
+using NHibernate.Dialect;
+using NHibernate.Driver;
+
+/*
  * Created by SharpDevelop.
  * User: Alexander
  * Date: 3/8/2013
@@ -6,10 +10,12 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-using System;
 
 namespace nh3test
 {
+    using System;
+    using NHibernate.Cfg;
+    
 	class Program
 	{
 		public static void Main(string[] args)
@@ -18,7 +24,24 @@ namespace nh3test
 			
 			// TODO: Implement Functionality Here
 			
+			Category categ = new Category();
+			categ.Id = 1;
+			CategoryMap catMap = new CategoryMap();
+			catMap.Id(x => x.Id);
 			
+			var cfg = new Configuration();
+			cfg.DataBaseIntegration(x => {
+			                            x.ConnectionString = "Server=localhost;Database=probe;Integrated Security=SSPI;";
+			                            x.Driver<SqlClientDriver>();
+			                            x.Dialect<MsSql2008Dialect>();
+			                        });
+			cfg.AddAssembly(Assembly.GetExecutingAssembly());
+			var sessionFactory = cfg.BuildSessionFactory();
+			using (var session = sessionFactory.OpenSession())
+		    using (var tx = session.BeginTransaction()) {
+		        // perform database logic
+		        tx.Commit();
+		    }
 			
 			
 			Console.Write("Press any key to continue . . . ");
