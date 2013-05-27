166815df17ddfe31ccfaf99f4a2cef97b67c1c69
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-05-27 09:08:32 +0400

    .

--- nh3test/nh3test/Program.cs
+++ nh3test/nh3test/Program.cs
@@ -10,7 +10,10 @@
 namespace nh3test
 {
     using System;
+    using FluentNHibernate.Cfg;
+    using FluentNHibernate.Cfg.Db;
     using NHibernate.Cfg;
+    using NHibernate.Tool.hbm2ddl;
     using System.Reflection;
     using NHibernate.Dialect;
     using NHibernate.Driver;
@@ -23,34 +26,67 @@ namespace nh3test
 			
 			// TODO: Implement Functionality Here
 			
+			try {
 			Category categ = new Category();
 			categ.Id = 1;
 			CategoryMap catMap = new CategoryMap();
 			catMap.Id(x => x.Id);
 			
-			var cfg = new Configuration();
-			cfg.DataBaseIntegration(x => {
-			                            x.ConnectionString = "Server=localhost;Database=probe;Integrated Security=SSPI;";
-			                            x.Driver<SqlClientDriver>();
-			                            x.Dialect<MsSql2008Dialect>();
-			                        });
-			cfg.AddAssembly(Assembly.GetExecutingAssembly());
-			var sessionFactory = cfg.BuildSessionFactory();
-			using (var session = sessionFactory.OpenSession())
-		    using (var tx = session.BeginTransaction()) {
-		        // perform database logic
-		        var customers = session.CreateCriteria<Customer>()
-		                               .List<Customer>();
-		        foreach (var customer in customers) {
-		            Console.WriteLine("{0} {1}", customer.FirstName, customer.LastName);
-		        }
-		        tx.Commit();
-		    }
-			Console.WriteLine("Press <ENTER> to exit...");
-			Console.ReadLine();
+			Product prod = new Product();
+			prod.Id = 1;
+			ProductMap prodMap = new ProductMap();
+			prodMap.Id(x => x.Id);
+			
+			string connString =
+			    "Server=spb8638\\FIRST;Database=probe;Integrated Security=SSPI;";
+			
+//			var cfg = new Configuration();
+//			cfg.DataBaseIntegration(x => {
+//			                            x.ConnectionString = "Server=spb8638\\FIRST;Database=probe;Integrated Security=SSPI;";
+//			                            x.Driver<SqlClientDriver>();
+//			                            x.Dialect<MsSql2008Dialect>();
+//			                        });
+//			cfg.AddAssembly(Assembly.GetExecutingAssembly());
+//			var sessionFactory = cfg.BuildSessionFactory();
+//			using (var session = sessionFactory.OpenSession())
+//		    using (var tx = session.BeginTransaction()) {
+//		        // perform database logic
+//		        var customers = session.CreateCriteria<Customer>()
+//		                               .List<Customer>();
+//		        foreach (var customer in customers) {
+//		            Console.WriteLine("{0} {1}", customer.FirstName, customer.LastName);
+//		        }
+//		        tx.Commit();
+//		    }
+//			Console.WriteLine("Press <ENTER> to exit...");
+//			Console.ReadLine();
+			
+			Fluently.Configure()
+			    .Database(MsSqlConfiguration
+			              .MsSql2008
+			              .ConnectionString(connString))
+			    .Mappings(m => m.FluentMappings.AddFromAssemblyOf<ProductMap>())
+			    .ExposeConfiguration(CreateSchema)
+			    .BuildConfiguration();
+			
+			
+			
+			}
+			catch (Exception eHZ) {
+			    Console.WriteLine(eHZ.Message);
+			}
+			
+			
 			
 			Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
+		}
+		
+		private static void CreateSchema(Configuration cfg)
+		{
+		    var schemaExport = new SchemaExport(cfg);
+		    schemaExport.Drop(false, true);
+		    schemaExport.Create(false, true);
 		}
 	}
 }
