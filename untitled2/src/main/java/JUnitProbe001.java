64e71dd95abb8e0eb1f10e053ee58cbc81c7aaa0
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2013-10-15 21:05:14 +0400

    .

---
+++ untitled2/src/main/java/JUnitProbe001.java
@@ -0,0 +1,39 @@
+//package com.example.foo;
+package com.example.foo;
+
+
+//import static org.junit.Assert.*; //.assertEquals;
+/*
+import static junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.junit.runners.*; //.JUnit4;
+*/
+/*
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.*;
+import java.io.File;
+import java.util.Dictionary;
+*/
+/**
+ * Tests for {@link Foo}.
+ *
+ * @author user@example.com (John Doe)
+ */
+/*
+@RunWith(JUnit4.class)
+public class JUnitProbe001 {
+
+    @Test
+    public void thisAlwaysPasses() {
+    }
+
+    @Test
+    @Ignore
+    public void thisIsIgnored() {
+    }
+}
+*/
