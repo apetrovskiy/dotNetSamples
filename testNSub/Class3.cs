8c220567f671df3002889d361b6ef05c41be26ec
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2013-11-04 00:12:51 +0400

    probe wrapper

---
+++ testNSub/Class3.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 11/3/2013
+ * Time: 11:28 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testNSub
+{
+	/// <summary>
+	/// Description of Class3.
+	/// </summary>
+	public class Class3
+	{
+		public Class3()
+		{
+		}
+		
+		public DamnSealedClass GetOneElement()
+		{
+			DamnSealedClass dc = MyFactory.GetElement();
+			DamnSealedCollection coll = dc.ManyElements();
+			if (null != coll && 0 < coll.Count) {
+				return coll[0];
+			} else {
+				return coll;
+			}
+		}
+	}
+}
---
+++ testNSub/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 11/2/2013
+ * Time: 12:42 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NSubstitute;
+using System.Windows.Automation;
+
+namespace testNSub
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			//AutomationElement ae = new AutomationElement();
+			
+			Class2 if2 = NSubstitute.Substitute.For<Class2>();
+			if2.Register();
+			//Interface2 if2 = NSubstitute.Substitute.For<Interface2>();
+			Class1 cl1 = new Class1(if2);
+			Interface2 if2returned = cl1.GetInternalObject();
+			Console.WriteLine(if2returned.IsRegistered);
+			Console.WriteLine(if2.Received().IsRegistered);
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
