f0b2d49fe8c9d3971da281883011c3847844d04d
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-11-07 21:28:35 +0400

    .

--- testWeb/testWeb/Program.cs
+++ testWeb/testWeb/Program.cs
@@ -6,10 +6,15 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-using System;
 
 namespace testWeb
 {
+    using System;
+    using System.Web;
+    using System.Net;
+    using System.Net.NetworkInformation;
+    //using System.Web.
+    
     class Program
     {
         public static void Main(string[] args)
@@ -17,6 +22,8 @@ namespace testWeb
             Console.WriteLine("Hello World!");
             
             // TODO: Implement Functionality Here
+            
+            var h = System.Web.HttpReq
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
