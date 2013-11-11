830f1bad43692cc2c46375a0fe1e5aa64c0d8adb
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-11-11 21:25:48 +0400

    .

---
+++ testHtmlWriter/testHtmlWriter/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 11/11/2013
+ * Time: 4:28 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testHtmlWriter
+{
+    using System;
+    using System.Web;
+    //using System.Web
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            //System.Web.
+            html
+            
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
