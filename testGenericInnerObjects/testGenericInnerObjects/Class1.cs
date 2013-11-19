6edf942372b03a2cae0b6ec11062f2acc23dac7b
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-11-19 12:00:20 +0400

    .

---
+++ testGenericInnerObjects/testGenericInnerObjects/Class1.cs
@@ -0,0 +1,30 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 11/18/2013
+ * Time: 10:52 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testGenericInnerObjects
+{
+    /// <summary>
+    /// Description of Class1.
+    /// </summary>
+    public class Class1//<T> where T : class
+    {
+        private T innerObject;
+        
+        public Class1(T innerObject)
+        {
+            this.innerObject = innerObject;
+        }
+        
+        public int GetNumber<T>()
+        {
+            return this.innerObject.GetNumber();
+        }
+    }
+}
