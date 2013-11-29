391c4479d8dcdc78f8c37c502a2165bdd9547e21
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2013-11-30 03:39:12 +0400

    .

---
+++ aopTest/aopTest/aaa.cs
@@ -0,0 +1,23 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 11/30/2013
+ * Time: 3:27 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using PostSharp.Aspects;
+
+namespace aopTest
+{
+	/// <summary>
+	/// Description of aaa.
+	/// </summary>
+	public class aaa : IMethodLevelAspect
+	{
+		public aaa()
+		{
+		}
+	}
+}
