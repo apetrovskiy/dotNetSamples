4d466223e9c99a5cc5e9a9c0f7829eeca699c5b8
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-12-12 11:19:12 +0400

    .

---
+++ testPsExtensionMethods/testPsExtensionMethods/Command3.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 12/7/2013
+ * Time: 12:18 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Management.Automation;
+
+namespace testPsExtensionMethods
+{
+    /// <summary>
+    /// Description of Command3.
+    /// </summary>
+    [Cmdlet(VerbsCommon.Get, "Command3")]
+    [OutputType(typeof(ILower))]
+    public class Command3 : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            ILower lower = new Lower();
+            lower.OutputString();
+        }
+    }
+}
---
+++ testPsExtensionMethods/testPsExtensionMethods/IUpper.cs
@@ -0,0 +1,19 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 12/7/2013
+ * Time: 12:09 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testPsExtensionMethods
+{
+    /// <summary>
+    /// Description of IUpper.
+    /// </summary>
+    public interface IUpper
+    {
+        int Number();
+    }
+}
