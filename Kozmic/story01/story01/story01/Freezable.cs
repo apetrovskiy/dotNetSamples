8e7b480e70f5541a0c4da6384f2b6bc35caf308a
Alexander Petrovskiy <uiautomation@hotmail.com>
2013-12-16 21:36:56 +0400

    .

---
+++ Kozmic/story01/story01/story01/Freezable.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 12/16/2013
+ * Time: 5:56 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Castle.DynamicProxy;
+
+namespace story01
+{
+    /// <summary>
+    /// Description of Freezable.
+    /// </summary>
+    public static class Freezable
+    {
+        private static readonly IDictionary<object, IFreezable> _freezables = new Dictionary<object, IFreezable>();
+        
+        private static readonly ProxyGenerator _generator = new ProxyGenerator();
+        
+        public static bool IsFreezable(object obj)
+        {
+            return obj != null && _freezables.ContainsKey(obj);
+        }
+          
+          
+        public static void Freeze(object freezable)
+        {
+            if (!IsFreezable(freezable))
+            {
+                throw new NotFreezableObjectException(freezable);
+            }
+            _freezables[freezable].Freeze();
+        }
+          
+        public static bool IsFrozen(object freezable)
+        {
+            return IsFreezable(freezable) && _freezables[freezable].IsFrozen;
+        }
+        
+        public static TFreezable MakeFreezable<TFreezable>() where TFreezable : class, new()
+        {
+            var freezableInterceptor = new FreezableInterceptor();
+            var proxy = _generator.CreateClassProxy<TFreezable>(new CallLoggingInterceptor(), freezableInterceptor);
+            _freezables.Add(proxy, freezableInterceptor);
+            return proxy;
+        }
+    }
+}
---
+++ Kozmic/story01/story01/story01/Pet.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 12/16/2013
+ * Time: 5:59 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace story01
+{
+    /// <summary>
+    /// Description of Pet.
+    /// </summary>
+    public class Pet
+    {
+        public virtual string Name { get; set; }
+        public virtual int Age { get; set; }
+        public virtual bool Deceased { get; set; }
+      
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Age: {1}, Deceased: {2}", Name, Age, Deceased);
+        }
+    }
+}
