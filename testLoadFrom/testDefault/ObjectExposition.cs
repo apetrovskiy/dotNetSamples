7f994f33f5a55acf6147220d5577fc80b251facf
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-01-08 13:37:11 +0400

    .

---
+++ testLoadFrom/testDefault/ObjectExposition.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 12/29/2013
+ * Time: 12:19 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testDefault
+{
+    using System;
+    using NSubstitute.Core;
+    
+    /// <summary>
+    /// Description of ObjectExposition.
+    /// </summary>
+    public class ObjectExposition
+    {
+        public void Test()
+        {
+            // var mock = new Mock<System.Windows.Forms.Form>();
+            // var driver = new FirefoxDriver();
+//            var mock = SubstituteFactory
+        }
+    }
+}
