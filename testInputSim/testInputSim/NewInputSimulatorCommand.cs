d75e6827d7e4fbe9a2e0c89692dd33a0b5cc42e1
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-01-17 23:27:43 +0400

    .

---
+++ testInputSim/testInputSim/NewInputSimulatorCommand.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 1/17/2014
+ * Time: 1:24 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testInputSim
+{
+    using System;
+    using System.Management.Automation;
+    using WindowsInput;
+    
+    /// <summary>
+    /// Description of GetInputSimulatorCommand.
+    /// </summary>
+    [Cmdlet(VerbsCommon.New, "InputSimulator")]
+    public class NewInputSimulatorCommand : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            WriteObject(new InputSimulator());
+        }
+    }
+}
