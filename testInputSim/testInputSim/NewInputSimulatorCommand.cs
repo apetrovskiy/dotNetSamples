e6a66d9fb50544f1fc2f5acb13e5ea089b44d072
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-01-19 03:52:14 +0400

    .

--- testInputSim/testInputSim/NewInputSimulatorCommand.cs
+++ testInputSim/testInputSim/NewInputSimulatorCommand.cs
@@ -12,6 +12,7 @@ namespace testInputSim
     using System;
     using System.Management.Automation;
     using WindowsInput;
+    using WindowsInput.Native;
     
     /// <summary>
     /// Description of GetInputSimulatorCommand.
@@ -27,8 +28,39 @@ namespace testInputSim
             // sim.Mouse.LeftButtonClick();
             // sim.Mouse.MoveMouseTo(double, double);
             // sim.Mouse.MoveMouseToPositionOnVirtualDesktop(double, double);
+            sim.Mouse.HorizontalScroll(int scrollAmountInClicks);
+            sim.Mouse.LeftButtonClick();
+            sim.Mouse.LeftButtonDoubleClick();
+            sim.Mouse.LeftButtonDown();
+            sim.Mouse.LeftButtonUp();
+            sim.Mouse.MoveMouseBy(int, int);
+            sim.Mouse.MoveMouseTo(double, double);
+            sim.Mouse.MoveMouseToPositionOnVirtualDesktop(double, double);
+            sim.Mouse.RightButtonClick();
+            sim.Mouse.RightButtonDoubleClick();
+            sim.Mouse.RightButtonDown();
+            sim.Mouse.RightButtonUp();
+            sim.Mouse.Sleep(int);
+            sim.Mouse.Sleep(TimeSpan);
+            sim.Mouse.VerticalScroll(int);
+            sim.Mouse.XButtonClick(int buttonId);
+            sim.Mouse.XButtonDoubleClick(int buttonId);
+            sim.Mouse.XButtonDown(int buttonId);
+            sim.Mouse.XButtonUp(int buttonId);
             
             // sim.Keyboard.TextEntry(string)
+            sim.Keyboard.KeyDown(VirtualKeyCode.VK_G);
+            sim.Keyboard.KeyPress(VirtualKeyCode.VK_G);
+            sim.Keyboard.KeyPress(VirtualKeyCode.VK_A, VirtualKeyCode.VK_B, VirtualKeyCode.VK_C);
+            sim.Keyboard.KeyUp(VirtualKeyCode.VK_G);
+            sim.Keyboard.ModifiedKeyStroke(new [] { VirtualKeyCode.SHIFT, VirtualKeyCode.CONTROL }, new [] { VirtualKeyCode.VK_G, VirtualKeyCode.VK_F });
+            sim.Keyboard.ModifiedKeyStroke(new [] { VirtualKeyCode.SHIFT, VirtualKeyCode.CONTROL }, VirtualKeyCode.VK_F);
+            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.SHIFT, new [] { VirtualKeyCode.VK_G, VirtualKeyCode.VK_F });
+            sim.Keyboard.ModifiedKeyStroke(VirtualKeyCode.SHIFT, VirtualKeyCode.VK_F);
+            sim.Keyboard.Sleep(1000);
+            // sim.Keyboard.Sleep(timespan);
+            sim.Keyboard.TextEntry("aaaaaaaaaaaa");
+            sim.Keyboard.TextEntry('b');
         }
     }
 }
---
+++ testNSub3/testNSub3/NewNSubCommand.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 1/18/2014
+ * Time: 10:59 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testNSub3
+{
+    using System.Management.Automation;
+    using System.Windows.Automation;
+    using UIAutomation;
+    using NSubstitute;
+    
+    /// <summary>
+    /// Description of NewNSubCommand.
+    /// </summary>
+    [Cmdlet(VerbsCommon.New, "NSub")]
+    public class NewNSubCommand : PSCmdlet
+    {
+        protected override void BeginProcessing()
+        {
+            var element = Substitute.For<IUiElement>();
+            element.Current.Name.Returns("nnn");
+            element.Current.AutomationId.Returns("auId");
+            element.Current.ControlType.Returns(ControlType.Button);
+            element.GetSupportedPatterns().Returns(new object[] { typeof(IInvokePattern), typeof(IValuePattern), typeof(ITableItemPattern) });
+            WriteObject(element);
+        }
+    }
+}
