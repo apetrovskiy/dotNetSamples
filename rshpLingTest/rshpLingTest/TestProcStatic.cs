7564b776b0ffdde606242873d5bc73ae84e30287
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-02-19 21:57:42 +0400

    .

---
+++ rshpLingTest/rshpLingTest/TestProcStatic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace rshpLingTest
+{
+    class TestProcStatic
+    {
+        public static TestProcImpl[] GetObjects(int id)
+        {
+            return new[]
+                {
+                    new TestProcImpl() {Id = id},
+                    new TestProcImpl() {Id = id},
+                    new TestProcImpl() {Id = id}
+                };
+        }
+    }
+}
