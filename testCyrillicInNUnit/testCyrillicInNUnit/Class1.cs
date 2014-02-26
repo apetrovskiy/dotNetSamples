2becbd07a2e14932e2636bffcc1080d317795fa9
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-02-26 22:00:40 +0400

    .

---
+++ testCyrillicInNUnit/testCyrillicInNUnit/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace testCyrillicInNUnit
+{
+    [TestFixture]
+    public class testClass
+    {
+
+        [SetUp]
+        public void Init()
+        {
+            Console.WriteLine("set up");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Console.WriteLine("tear down");
+        }
+
+        [Test]
+        [TestCase("Погода")]
+        public void TransferFunds(string weather)
+        {
+            Console.WriteLine("test 01");
+        }
+
+        [Test]
+        [TestCase(@"Погода2")]
+        public void TransferFunds2(string weather)
+        {
+            Console.WriteLine("test 02");
+        }
+    }
+}
---
+++ testInheritanceWithIfaces/testInheritanceWithIfaces/ITest.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 2/20/2014
+ * Time: 6:57 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testInheritanceWithIfaces
+{
+	/// <summary>
+	/// Description of ITest.
+	/// </summary>
+	public interface ITest
+	{
+		void InterfaceMethod();
+		int InterfaceProperty { get; set; }
+	}
+}
