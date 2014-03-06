d682c0995e885950b8802c23e162a85d08ec7155
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-03-06 14:11:35 +0400

    .

---
+++ testSameNamespace/assembly02/Element.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 3/6/2014
+ * Time: 12:12 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Same.Namespace
+{
+    /// <summary>
+    /// Description of Element.
+    /// </summary>
+    public class Element
+    {
+        public int GetValue()
+        {
+            return 5;
+        }
+        
+        public string StrData { get; set; }
+    }
+}
