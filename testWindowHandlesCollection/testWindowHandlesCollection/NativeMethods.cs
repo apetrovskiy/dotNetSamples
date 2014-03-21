8cfe65a1dd1304dbcbe996b217bbd67b3b41040a
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-03-21 21:59:49 +0400

    .

---
+++ testWindowHandlesCollection/testWindowHandlesCollection/NativeMethods.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 3/19/2014
+ * Time: 3:17 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testWindowHandlesCollection
+{
+    /// <summary>
+    /// Description of NativeMethods.
+    /// </summary>
+    public class NativeMethods
+    {
+        public NativeMethods()
+        {
+        }
+    }
+}
