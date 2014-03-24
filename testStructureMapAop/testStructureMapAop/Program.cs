97285a9ff6a1737434796f9afb3aeefd8435b017
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-03-24 22:41:07 +0400

    .

---
+++ testStructureMapAop/testStructureMapAop/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: APetrovsky
+ * Date: 3/24/2014
+ * Time: 7:16 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testStructureMapAop
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
