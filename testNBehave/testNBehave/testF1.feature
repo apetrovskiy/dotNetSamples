305d46aec84193bf20b97e3c4bf37a122506d183
apetrovskiy <apetrovskiy@hotmail.com>
2014-05-16 23:03:03 +0400

    .

---
+++ testNBehave/testNBehave/testF1.feature
@@ -0,0 +1,11 @@
+﻿Feature: a small example with tables and examples
+	As a user
+	I want to have something
+	So that I have that something
+
+# This is a comment line and will be ignored.
+	
+Scenario: Add item to list
+	Given an empty list
+	When I add foo to list
+	Then the list should contain foo
---
+++ testNSpec/testNSpec/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 5/13/2014
+ * Time: 9:55 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using NSpec.Domain;
+
+namespace testNSpec
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
