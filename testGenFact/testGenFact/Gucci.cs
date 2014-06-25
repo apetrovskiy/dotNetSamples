19d80b14731a79a2cebcddfdab014c36bb65f524
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-06-25 05:21:07 +0400

    .

---
+++ testGenFact/testGenFact/Gucci.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 6/20/2014
+ * Time: 9:32 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testGenFact
+{
+    /// <summary>
+    /// Description of Gucci.
+    /// </summary>
+    public class Gucci : IBrand
+    {
+        public int Price { get { return 1000; } }
+        public string Material { get { return "Crocodile skin"; } }
+    }
+}
---
+++ testNbehaveFull/testLib/HelloWorld.feature
@@ -0,0 +1,7 @@
+﻿Feature: HelloWorld
+  A narrative is optional
+
+Scenario: Login
+  Given I am not logged in
+  When I log in as Morgan with a password SecretPassw0rd
+  Then I should see a message, "Welcome, Morgan!"
