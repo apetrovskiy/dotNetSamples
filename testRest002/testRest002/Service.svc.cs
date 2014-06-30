6cc7fda78bc7d93c73ab09cc4b0d4250f8dee241
apetrovskiy <apetrovskiy@hotmail.com>
2014-06-30 21:43:45 +0400

    .

---
+++ testRest002/testRest002/Service.svc.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 6/30/2014
+ * Time: 9:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace testRest002
+{
+	[ServiceContract]
+	public interface IService
+	{
+	   [OperationContract]
+	   [WebGet(UriTemplate = "operation/{name}")]
+	   string MyOperation(string name);
+	}
+	
+	public class Service : IService
+	{
+	   public string MyOperation(string name)
+	   {
+		  // implement the operation
+		  return string.Format("Operation name: {0}", name);
+	   }
+	} 
+}
