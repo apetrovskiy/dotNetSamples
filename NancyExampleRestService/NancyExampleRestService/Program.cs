4f84380d99e5283ce45fd0262922630698f9c3d1
apetrovskiy <apetrovskiy@hotmail.com>
2014-07-03 19:10:03 +0400

    .

---
+++ NancyExampleRestService/NancyExampleRestService/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 7/3/2014
+ * Time: 1:03 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace NancyExampleRestService
+{
+	using System;
+	using Nancy.Hosting.Self;
+	
+	class Program
+	{
+		const string url = "http://localhost:12345";
+		
+		public static void Main(string[] args)
+		{
+			// create a new self-host server
+			var nancyHost = new NancyHost(new Uri(url));
+			// start
+			nancyHost.Start();
+			Console.WriteLine("REST service listening on " + url);
+			//stop with an <Enter> ket press
+			Console.ReadLine();
+			nancyHost.Stop();
+		}
+	}
+}
