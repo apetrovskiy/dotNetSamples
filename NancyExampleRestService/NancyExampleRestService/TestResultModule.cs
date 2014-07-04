f3dea9cedcdc631b88a7799d008e4a691b8af771
Alexander Petrovskiy <uiautomation@hotmail.com>
2014-07-04 12:21:35 +0400

    a simple REST application over Nancy

---
+++ NancyExampleRestService/NancyExampleRestService/TestResultModule.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Alexander
+ * Date: 7/4/2014
+ * Time: 11:27 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace NancyExampleRestService
+{
+    using System;
+	using Nancy;
+    
+    /// <summary>
+    /// Description of TestResultModule.
+    /// </summary>
+    public class TestResultModule : NancyModule
+    {
+        public TestResultModule() : base("/TestResults")
+        {
+            // Get["/{id}/{name}/{status}"] = parameter => { return GetTestResult(parameter.id, parameter.name, parameter.status); };
+            Get["/{id}/{name}/{status}"] = parameter => {
+                var result = new { Id = parameter.id, Name = parameter.name, Status = parameter.status };
+                return Response.AsJson(result).WithStatusCode(HttpStatusCode.OK);
+            };
+            
+            Post["/{id}/{name}/{status}"] = parameter => {
+                var result = new { Id = parameter.id, Name = parameter.name, Status = parameter.status };
+                return Response.AsJson(result).WithStatusCode(HttpStatusCode.OK); 
+            };
+        }
+//        
+//        private object GetTestResult(int id, string name, int status)
+//        {
+//            // fake a return
+//            return new { Id = id, Name = name, Status = status };
+//        }
+    }
+}
