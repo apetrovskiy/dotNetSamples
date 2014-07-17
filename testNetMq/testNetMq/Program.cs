5fc5094b12330acc1bfb2984d9f2e7de0dfea007
apetrovskiy <apetrovskiy@hotmail.com>
2014-07-17 22:02:05 +0400

    .

---
+++ testNetMq/testNetMq/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 7/16/2014
+ * Time: 5:17 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testNetMq
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
