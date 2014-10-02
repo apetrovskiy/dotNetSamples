9ee6b3c6c48317c4535e4e0cd36176a1793a1781
apetrovskiy <apetrovskiy@hotmail.com>
2014-10-02 15:52:57 +0400

    .

---
+++ testAsmPath/testAsmPath/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 10/1/2014
+ * Time: 12:39 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testAsmPath
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            foreach (var asm in AppDomain.CurrentDomain.GetAssemblies()) {
+                Console.WriteLine(asm.CodeBase);
+                Console.WriteLine(asm.EscapedCodeBase);
+                Console.WriteLine(asm.FullName);
+                Console.WriteLine(asm.Location);
+            }
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
