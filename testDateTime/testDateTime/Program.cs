3b4a58585751c769f220b654cab6487036601e7f
apetrovskiy <apetrovskiy@hotmail.com>
2014-11-09 15:57:59 +0300

    .

---
+++ testDateTime/testDateTime/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 11/9/2014
+ * Time: 3:48 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testDateTime
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            var dateTimeAsString = "2014-11-08T15:36:44.4134433-08:00";
+            var dateTime = Convert.ToDateTime(dateTimeAsString);
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ testUserAuthForNancy/testUserAuthForNancy/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 11/7/2014
+ * Time: 8:20 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testUserAuthForNancy
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // Environment.UserDomainName
+            // Environment.UserName
+            // Environment.MachineName
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
