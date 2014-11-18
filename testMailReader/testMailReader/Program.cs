d14efc9147818542e9edc5bfe60f1084b882b23e
apetrovskiy <apetrovskiy@hotmail.com>
2014-11-19 01:08:52 +0300

    new file:   testMailReader/testMailReader.sln 	new file:   testMailReader/testMailReader/Program.cs 	new file:   testMailReader/testMailReader/Properties/AssemblyInfo.cs 	new file:   testMailReader/testMailReader/testMailReader.csproj

---
+++ testMailReader/testMailReader/Program.cs
@@ -0,0 +1,43 @@
+/*
+using OpenPop.Pop3;
+using OpenPop.Common.Logging;
+using OpenPop.Mime;
+using OpenPop.Pop3.Exceptions;
+*/
+
+namespace testMailReader
+{
+    using System;
+    using ActiveUp.Net.Imap4;
+    using ActiveUp.Net.Mail;
+
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+            /*
+            var client = new Pop3Client();
+            client.Connect("pop.gmail.com", 995, true);
+            client.Authenticate("admin@bendytree.com", "YourPasswordHere", AuthenticationMethod.Auto);
+            var count = client.GetMessageCount();
+            Message message = client.GetMessage(count);
+            Console.WriteLine(message.Headers.Subject);
+            */
+
+            var pop3Client = new Pop3Client ();
+            // pop3Client.ConnectSsl("pop.gmail.com", 995, "admin@bendytree.com", "YourPasswordHere");
+            // pop3Client.L
+            // Console.WriteLine (pop3Client.MessageCount);
+            // pop3Client.RetrieveMessage (1);
+
+            var imapClient = new Imap4Client ();
+            imapClient.ConnectSsl("imap.gmail.com", 993);
+            imapClient.Login ("admin@bendytree.com", "YourPasswordHere");
+
+            // var inbox = imapClient.SelectMailbox ("inbox");
+
+            Console.ReadKey ();
+        }
+    }
+}
