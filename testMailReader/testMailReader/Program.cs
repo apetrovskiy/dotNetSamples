56661f557578d628780354aa11a8ab5f9d572e22
apetrovskiy <apetrovskiy@hotmail.com>
2014-11-19 21:53:17 +0300

    ю

--- testMailReader/testMailReader/Program.cs
+++ testMailReader/testMailReader/Program.cs
@@ -1,9 +1,7 @@
-/*
 using OpenPop.Pop3;
 using OpenPop.Common.Logging;
 using OpenPop.Mime;
 using OpenPop.Pop3.Exceptions;
-*/
 
 namespace testMailReader
 {
@@ -16,24 +14,26 @@ namespace testMailReader
         public static void Main (string[] args)
         {
             Console.WriteLine ("Hello World!");
-            /*
-            var client = new Pop3Client();
-            client.Connect("pop.gmail.com", 995, true);
-            client.Authenticate("admin@bendytree.com", "YourPasswordHere", AuthenticationMethod.Auto);
+            
+            var client = new OpenPop.Pop3.Pop3Client();
+            // client.Connect("pop.gmail.com", 995, true);
+            client.Connect("192.168.129.21", 25, false);
+            client.Authenticate("report_reader@SPALab.at.local", "Lock12Lock", AuthenticationMethod.Auto);
             var count = client.GetMessageCount();
-            Message message = client.GetMessage(count);
-            Console.WriteLine(message.Headers.Subject);
-            */
+//            Message message = client.GetMessage(count);
+//            Console.WriteLine(message.Headers.Subject);
+            
 
             var pop3Client = new Pop3Client ();
             // pop3Client.ConnectSsl("pop.gmail.com", 995, "admin@bendytree.com", "YourPasswordHere");
+            pop3Client.Connect("192.168.129.21", 25, "report_reader@SPALab.at.local", "Lock12Lock");
             // pop3Client.L
             // Console.WriteLine (pop3Client.MessageCount);
-            // pop3Client.RetrieveMessage (1);
+            pop3Client.RetrieveMessage (1);
 
             var imapClient = new Imap4Client ();
             imapClient.ConnectSsl("imap.gmail.com", 993);
-            imapClient.Login ("admin@bendytree.com", "YourPasswordHere");
+            imapClient.Login ("report_reader@SPALab.at.local", "Lock12Lock");
 
             // var inbox = imapClient.SelectMailbox ("inbox");
 
