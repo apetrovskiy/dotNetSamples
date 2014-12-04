0267f020aab8d85accb1ad53a43d0fa285b8b036
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-04 19:56:49 +0300

    .

---
+++ testDotLiquidAlone/testDotLiquidAlone/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 12/4/2014
+ * Time: 7:55 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testDotLiquidAlone
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
