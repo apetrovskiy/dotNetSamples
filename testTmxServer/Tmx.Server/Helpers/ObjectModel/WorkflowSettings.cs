9a212638e6fddb31901bce2096e90aafe5c9a67f
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-05 01:42:08 +0300

    modified:   testTmxServer/Tmx.Interfaces/Tmx.Interfaces.csproj 	new file:   testTmxServer/Tmx.Server/Helpers/ObjectModel/TestRunSettings.cs 	new file:   testTmxServer/Tmx.Server/Helpers/ObjectModel/WorkflowSettings.cs 	modified:   testTmxServer/Tmx.Server/Tmx.Server.csproj

---
+++ testTmxServer/Tmx.Server/Helpers/ObjectModel/WorkflowSettings.cs
@@ -0,0 +1,21 @@
+/*
+ * Created by SharpDevelop.
+ * User: Alexander Petrovskiy
+ * Date: 12/05/2014
+ * Time: 0:28 AM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace Tmx.Server
+{
+    using System;
+
+    public class WorkflowSettings : ISettings
+    {
+        public WorkflowSettings ()
+        {
+        }
+    }
+}
+
