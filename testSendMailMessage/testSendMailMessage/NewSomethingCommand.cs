d0eb3cb64938437afec4b8296431fc60bd72d179
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-04 19:54:25 +0300

    .

---
+++ testSendMailMessage/testSendMailMessage/NewSomethingCommand.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 12/4/2014
+ * Time: 5:30 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testSendMailMessage
+{
+    /// <summary>
+    /// Description of NewSomethingCommand.
+    /// </summary>
+    public class NewSomethingCommand
+    {
+        public NewSomethingCommand()
+        {
+        }
+    }
+}
---
+++ testSendMailMessage/testSendMailMessage/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 12/4/2014
+ * Time: 5:29 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testSendMailMessage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // TODO: Implement Functionality Here
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
