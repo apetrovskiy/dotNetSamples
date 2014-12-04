285e18ff44eb446fbef009132409489eec3836d0
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-04 23:06:47 +0300

    .

--- testDotLiquidAlone/testDotLiquidAlone/Program.cs
+++ testDotLiquidAlone/testDotLiquidAlone/Program.cs
@@ -6,10 +6,13 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-using System;
 
-namespace testDotLiquidAlone
+namespace NancyExamples
 {
+    using System;
+    using System.IO;
+    using DotLiquid;
+    
     class Program
     {
         public static void Main(string[] args)
@@ -17,6 +20,35 @@ namespace testDotLiquidAlone
             Console.WriteLine("Hello World!");
             
             // TODO: Implement Functionality Here
+            
+            Template.RegisterSafeType(typeof(Product), new[] { "Name", "Price", "Description" });
+            Template.RegisterSafeType(typeof(Product2), new[] { "Name", "Price", "Description", "Id", "Number" });
+            Template.RegisterSafeType(typeof(Product3), new[] { "Name", "Price", "Description", "Id", "Number", "UniqueId" });
+            Template.RegisterSafeType(typeof(ProductList), new[] { "Products" });
+            Template.RegisterSafeType(typeof(Guid), member => member.ToString());
+            
+            Template template;
+            using (var reader = new StreamReader(@"./Views/sorted.liquid")) {
+                var templateCode = reader.ReadToEnd();
+                template = Template.Parse(templateCode);
+                reader.Close();
+            }
+            
+            var list = new ProductList ();
+            list.Products.AddRange (
+                new [] {
+                new Product { Name = "c", Price = 5, Description = "one" },
+                new Product { Name = "a", Price = 10, Description = "two" },
+                new Product { Name = "z", Price = 1, Description = "three" },
+                new Product { Name = "b", Price = 11, Description = "four" },
+                new Product { Name = "a", Price = 8, Description = "five" },
+                new Product { Name = "z", Price = 9, Description = "six" },
+                new Product { Name = "k", Price = 11, Description = "seven" },
+                new Product { Name = "h", Price = 5, Description = "eight" }
+            });
+            string result = template.Render(Hash.FromAnonymousObject(new { @Model = list }));
+            
+            Console.WriteLine(result);
             
             Console.Write("Press any key to continue . . . ");
             Console.ReadKey(true);
