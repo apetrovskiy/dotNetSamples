4eb4d37412c33e1736a0bb04811f2f14fbd6bed7
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-12 02:35:59 +0300

    new file:   testChartApi02/packages/GoogleChartSharp.dll 	new file:   testChartApi02/packages/GoogleChartSharp.xml 	new file:   testChartApi02/testChartApi02.sln 	new file:   testChartApi02/testChartApi02/MainWindow.cs 	new file:   testChartApi02/testChartApi02/Program.cs 	new file:   testChartApi02/testChartApi02/Properties/AssemblyInfo.cs 	new file:   testChartApi02/testChartApi02/gtk-gui/MainWindow.cs 	new file:   testChartApi02/testChartApi02/gtk-gui/generated.cs 	new file:   testChartApi02/testChartApi02/gtk-gui/gui.stetic 	new file:   testChartApi02/testChartApi02/testChartApi02.csproj 	new file:   testF/testF.sln 	new file:   testF/testF/Program.cs 	new file:   testF/testF/Properties/AssemblyInfo.cs 	new file:   testF/testF/testF.csproj 	new file:   testMonoDevelop5/testMonoDevelop5.sln 	new file:   testMonoDevelop5/testMonoDevelop5/Program.cs 	new file:   testMonoDevelop5/testMonoDevelop5/Properties/AssemblyInfo.cs 	new file:   testMonoDevelop5/testMonoDevelop5/testMonoDevelop5.csproj 	modified:   testTmxServer/Tmx.Client/Tmx.Client.csproj 	modified:   testTmxServer/Tmx.Core/Tmx.Core.csproj 	modified:   testTmxServer/Tmx.Interfaces/Tmx.Interfaces.csproj 	modified:   testTmxServer/Tmx.Server/Tmx.Server.csproj 	modified:   testTmxServer/testTmxServer.sln 	modified:   testTmxServer/testTmxServer/testTmxServer.csproj

---
+++ testF/testF/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace testF
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
---
+++ testMonoDevelop5/testMonoDevelop5/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace testMonoDevelop5
+{
+    using System;
+    using System.Management.Instrumentation;
+
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
