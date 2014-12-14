527dbbdf64326cac1043bf8ad2d682c6d3e736a7
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-15 00:46:29 +0300

    new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites-00.sln 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Controllers/HomeController.cs 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Global.asax 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Global.asax.cs 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/PartyInvites-00.csproj 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Views/Home/Index.aspx 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Views/Shared/Error.aspx 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Views/Web.config 	new file:   Pro ASP.NET MVC5/PartyInvites-00/PartyInvites/Web.config 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites.sln 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/Controllers/HomeController.cs 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/Global.asax 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/Global.asax.cs 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/PartyInvites.csproj 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/Views/Web.config 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/Web.config 	new file:   Pro ASP.NET MVC5/PartyInvites/PartyInvites/obj/Debug/.NETFramework,Version=v4.5.AssemblyAttribute.cs 	new file:   testAspNetWithTests/testAspNetWithTests.Tests/Controllers/HomeControllerTest.cs 	new file:   testAspNetWithTests/testAspNetWithTests.Tests/testAspNetWithTests.Tests.csproj 	new file:   testAspNetWithTests/testAspNetWithTests.sln 	new file:   testAspNetWithTests/testAspNetWithTests/Controllers/HomeController.cs 	new file:   testAspNetWithTests/testAspNetWithTests/Global.asax 	new file:   testAspNetWithTests/testAspNetWithTests/Global.asax.cs 	new file:   testAspNetWithTests/testAspNetWithTests/Views/Home/Index.aspx 	new file:   testAspNetWithTests/testAspNetWithTests/Views/Shared/Error.aspx 	new file:   testAspNetWithTests/testAspNetWithTests/Views/Web.config 	new file:   testAspNetWithTests/testAspNetWithTests/Web.config 	new file:   testAspNetWithTests/testAspNetWithTests/obj/Debug/.NETFramework,Version=v4.5.AssemblyAttribute.cs 	new file:   testAspNetWithTests/testAspNetWithTests/testAspNetWithTests.csproj

---
+++ Pro ASP.NET MVC5/PartyInvites/PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+// using System.Web.Mvc.Ajax;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index() {
+            return "Hello World";
+        }
+    }
+}
+
