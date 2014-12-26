f2126c9fd491f6910da41ad508fb250af2e74cb4
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-26 13:50:33 +0300

    .

--- testHtmlLetterParser/testHtmlLetterParser/Program.cs
+++ testHtmlLetterParser/testHtmlLetterParser/Program.cs
@@ -20,9 +20,34 @@ namespace testHtmlLetterParser
             // var whatCollection = doc.DocumentNode.SelectNodes ("//td");
             // var whatCollection = doc.DocumentNode.SelectNodes ("//td[@class]");
             // var whatCollection = doc.DocumentNode.SelectNodes ("//td[@id]");
-            var whatCollection = doc.DocumentNode.SelectNodes ("//td[@id]").Where(node => node.Id == "What");
-            whatCollection.ToList().ForEach(node => Console.WriteLine (node.Id));
-            Console.WriteLine (whatCollection.Count);
+            // this works
+            // var whatCollection = doc.DocumentNode.SelectNodes ("//td[@id]").Where(node => node.Id == "What");
+            // this works
+            // var whatCollection = doc.DocumentNode.SelectNodes ("//td[@id='What']|//td[@id='Who']");
+            var whatCollection = doc.DocumentNode.SelectNodes("//td[@id='Action']");
+            whatCollection.ToList().ForEach(node => {
+                                                Console.WriteLine ("id={0}, text={1}", node.Id ?? string.Empty, node.InnerText ?? string.Empty);
+                                                var nextNode = node.NextSibling;
+                                                try {
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                    nextNode = nextNode.NextSibling;
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                    nextNode = nextNode.NextSibling;
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                    nextNode = nextNode.NextSibling;
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                    nextNode = nextNode.NextSibling;
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                    nextNode = nextNode.NextSibling;
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                    nextNode = nextNode.NextSibling;
+                                                    Console.WriteLine("id={0}, text={1}", nextNode.Id ?? string.Empty, nextNode.InnerText ?? string.Empty);
+                                                }
+                                                catch (Exception ee) {
+                                                    Console.WriteLine(ee.Message);
+                                                }
+                                            });
+            Console.WriteLine (whatCollection.Count());
 
             Console.ReadKey ();
         }
