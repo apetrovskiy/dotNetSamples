d3ec13235a6d182053f7dd06064d1f8f37017820
apetrovskiy <apetrovskiy@hotmail.com>
2014-12-26 03:36:29 +0300

    new file:   testDbLinq/packages/repositories.config 	new file:   testDbLinq/testDbLinq.sln 	new file:   testDbLinq/testDbLinq/App.config 	new file:   testDbLinq/testDbLinq/Program.cs 	new file:   testDbLinq/testDbLinq/Properties/AssemblyInfo.cs 	new file:   testDbLinq/testDbLinq/testDbLinq.csproj 	new file:   testHtmlLetterParser/letters/MailDump_SPLab-2008R2.SPALab.at.local.htm 	new file:   testHtmlLetterParser/letters/MailDump_SPLab-2012.SPALab.at.local.htm 	new file:   testHtmlLetterParser/letters/MailDump_SPLab-2012R2.SPALab.at.local.htm 	new file:   testHtmlLetterParser/letters/MailDump_SPLab-7x86.SPALab.at.local.htm

---
+++ testDbLinq/testDbLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testDbLinq
+{
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
