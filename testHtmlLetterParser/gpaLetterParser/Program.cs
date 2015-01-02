42d365332aca01a9aa9a89acf05fa94e2760aa18
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-02 14:17:53 +0300

    modified:   testHtmlLetterParser/gpaLetterParser/Program.cs

--- testHtmlLetterParser/gpaLetterParser/Program.cs
+++ testHtmlLetterParser/gpaLetterParser/Program.cs
@@ -1,12 +1,30 @@
-﻿using System;
-
+﻿
 namespace gpaLetterParser
 {
+    using System;
+    using System.Linq;
+    using HtmlAgilityPack;
+    using testHtmlLetterParser;
+
     class MainClass
     {
         public static void Main (string[] args)
         {
-            // C:\Projects\probe\testHtmlLetterParser\letters\gpa_errors_2008R2.htm 
+            // C:\Projects\probe\testHtmlLetterParser\letters\gpa_errors_2008R2.htm
+
+            var doc = new HtmlDocument();
+            doc.Load(@"../../../letters/gpa_errors_2008R2.htm");
+
+            var tableProcessor = new TableProcessor(
+                doc.DocumentNode.SelectNodes ("//table").First(),
+                // "//table//td[. = 'User name']|//table//td[. = 'Email']|//table//td[. = 'Expires in']",
+                ".",
+                "",
+                ".");
+            tableProcessor.Process ();
+            var list = tableProcessor.GetCollection ();
+            tableProcessor.ExportCsv ("/home/alexander/Documents/gpa_errors.txt");
+
             Console.WriteLine ("Hello World!");
         }
     }
