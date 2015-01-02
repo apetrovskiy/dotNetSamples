4460f481985a0e50cd9ef15d93ee687bbda82960
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-03 01:14:41 +0300

    new file:   testHtmlLetterParser/letters/wsa_001.html 	modified:   testHtmlLetterParser/testHtmlLetterParser/TableProcessor.cs

--- testHtmlLetterParser/testHtmlLetterParser/TableProcessor.cs
+++ testHtmlLetterParser/testHtmlLetterParser/TableProcessor.cs
@@ -56,16 +56,6 @@ namespace testHtmlLetterParser
                 int counter = 0;
                 var tdNodes = rowNode.SelectNodes(".//td");
                 if (null != tdNodes && ColumnHeaders.Count() == tdNodes.Count) {
-                    // ColumnHeaders.Select(headerNode => headerNode.InnerText).ToList().ForEach(headerName => {
-                    
-//                    foreach (var hNode in ColumnHeaders) {
-//                        var headerText = hNode.SelectNodes(_customColumnHeaderExpression).FirstOrDefault().InnerText.Trim();
-//                    }
-//                    var headerNodes = ColumnHeaders.Select(headerNode => headerNode.SelectNodes(_customColumnHeaderExpression));
-//                    var headerNode = headerNodes.FirstOrDefault().InnerText.Trim();
-//                    var temp01 = (headerNodes.FirstOrDefault().InnerText.Trim()).ToList();
-                    
-                    
                     ColumnHeaders.Select(headerNode => headerNode.SelectNodes(_customColumnHeaderExpression).FirstOrDefault().InnerText.Trim()).ToList().ForEach(headerName => {
                         dict.Add(headerName, tdNodes[counter].SelectNodes(_customRowItemsExpression).FirstOrDefault().InnerText.Trim());
                         counter++;
@@ -79,15 +69,11 @@ namespace testHtmlLetterParser
         
         IEnumerable<HtmlNode> getColumnHeaders()
         {
-//            if (!string.IsNullOrEmpty (_customColumnHeaderExpression))
-//                return _tableNode.SelectNodes (_customColumnHeaderExpression);
             return _useFirstRowAsHeaders ? getColumnHeadersAsFirstRow() : getColumnHeadersAsElementsTh();
         }
         
         IEnumerable<HtmlNode> getColumnHeadersAsElementsTh()
         {
-            // this works
-            // return _tableNode.Descendants().Where(node => node.OriginalName == "th"); // ??
             return _tableNode.SelectNodes(".//th"); // ??
         }
         
@@ -98,20 +84,16 @@ namespace testHtmlLetterParser
         
         IEnumerable<HtmlNode> getRows()
         {
-//            if (!string.IsNullOrEmpty (_customRowsExpression))
-//                return _tableNode.SelectNodes (_customRowsExpression);
             return _useFirstRowAsHeaders ? getAllRowsButFirst() : getAllRows();
         }
         
         IEnumerable<HtmlNode> getAllRows()
         {
-            // return _tableNode.Descendants ().Where (node => node.OriginalName == "tr");
             return _tableNode.SelectNodes (".//tr");
         }
         
         IEnumerable<HtmlNode> getAllRowsButFirst()
         {
-            // return _tableNode.Descendants ().Where (node => node.OriginalName == "tr").Skip(1);
             return _tableNode.SelectNodes (".//tr").Skip (1);
         }
         
@@ -119,7 +101,6 @@ namespace testHtmlLetterParser
         {
             var headerItems = string.Empty;
             ColumnHeaders.ToList ().ForEach (node =>  {
-                // headerItems += node.InnerText.Trim ();
                 headerItems += "\"";
                 headerItems += node.SelectNodes(_customColumnHeaderExpression).FirstOrDefault().InnerText.Trim ();
                 headerItems += "\",";
@@ -142,8 +123,6 @@ namespace testHtmlLetterParser
             if (null == tdNode.SelectNodes (_customRowItemsExpression))
                 return string.Empty;
             var tdData = tdNode.SelectNodes (_customRowItemsExpression).FirstOrDefault ().InnerText.Trim ();
-//            if (string.IsNullOrEmpty (tdData))
-//                return string.Empty;
             return "\"" + tdData + "\",";
         }
     }
