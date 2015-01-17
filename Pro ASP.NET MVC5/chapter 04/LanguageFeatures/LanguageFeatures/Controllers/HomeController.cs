a25c58ef69e246cbef68f7b70a2d2d31041991d2
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-17 03:11:18 +0300

    .

---
+++ Pro ASP.NET MVC5/chapter 04/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        /*
+        public ActionResult Index()
+        {
+            return View();
+        }
+        */
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            Product myProduct = new Product();
+            myProduct.Name = "Kayak";
+            string productName = myProduct.Name;
+            return View("Result", (object)string.Format("Product name: {0}", productName));
+        }
+
+        public ViewResult CreateProduct()
+        {
+            Product myProduct = new Product
+            {
+                ProductId = 100, Name = "Kayak",
+                Description = "A boat for one person",
+                Price = 275M, Category = "Watersports"
+            };
+            return View("Result", (object)string.Format("Category: {0}", myProduct.Category));
+        }
+
+        public ViewResult CreateCollection()
+        {
+            string[] stringArray = { "apple", "orange", "plum" };
+            List<int> intList = new List<int> { 10, 20, 30, 40 };
+            Dictionary<string, int> myDict = new Dictionary<string, int> {
+                { "apple", 10 }, { "orange", 20 }, { "plum", 30 }
+            };
+            return View("Result", (object)stringArray[1]);
+        }
+
+        public ViewResult UseExtension()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = new List<Product>
+                {
+                    new Product { Name = "Kayak", Price = 275M },
+                    new Product { Name = "Lifejacket", Price = 48.95M },
+                    new Product { Name = "Soccer ball", Price = 19.50M },
+                    new Product { Name = "Corner flag", Price = 34.95M }
+                }
+            };
+            decimal cartTotal = cart.TotalPrices();
+            return View("Result", (object)string.Format("Total: {0:c}", cartTotal));
+        }
+    }
+}
---
+++ Pro ASP.NET MVC5/chapter 04/LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+        string name;
+        public int ProductId { get; set; }
+        public string Name
+        {
+            get { return ProductId + name; }
+            set { name = value; }
+        }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+    }
+}
