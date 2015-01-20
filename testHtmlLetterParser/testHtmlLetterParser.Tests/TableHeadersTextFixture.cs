9f968410adde0b431d4d999e81c2a2dc1f3f738b
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-20 23:56:04 +0300

    modified:   testHtmlLetterParser/testHtmlLetterParser.Tests/TableHeadersTextFixture.cs

--- testHtmlLetterParser/testHtmlLetterParser.Tests/TableHeadersTextFixture.cs
+++ testHtmlLetterParser/testHtmlLetterParser.Tests/TableHeadersTextFixture.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using HtmlAgilityPack;
 using NUnit.Framework;
+using Xunit;
 using testHtmlLetterParser;
 
 namespace testHtmlLetterParter.Tests
@@ -9,105 +10,106 @@ namespace testHtmlLetterParter.Tests
     [TestFixture]
     public class TableHeadersTextFixture
     {
-        public TableHeadersTextFixture ()
+        TableProcessor _tableProcessor;
+        HtmlDocument _document;
+
+        public TableHeadersTextFixture()
         {
+            SetUp ();
         }
 
         [SetUp]
         public void SetUp()
         {
-
+            _tableProcessor = null;
+            _document = null;
         }
 
-        [TearDown]
-        public void TearDown()
-        {
-
-        }
-
-        [Test]
+        [Test][Fact]
         public void HeadersFromThNodes()
         {
-            var doc = GIVEN_HtmlDocument (Page.TableWithThNodes);
-            var tableProcessor = new TableProcessor(doc.DocumentNode.SelectNodes("//table").First());
-            Assert.AreEqual(true, tableProcessor.Ready);
-            Assert.AreEqual (2, tableProcessor.ColumnHeaders.Count ());
-            Assert.AreEqual (2, tableProcessor.ColumnHeaderNames.Count ());
-            Assert.AreEqual ("Col1", tableProcessor.ColumnHeaderNames.First ());
-            Assert.AreEqual ("Col2", tableProcessor.ColumnHeaderNames.Last ());
-            Assert.AreEqual(1, tableProcessor.Rows.Count());
-            
-            Console.WriteLine("======================================= HeadersFromThNodes =======================================");
-            foreach(var headerName in tableProcessor.ColumnHeaderNames) {
-                Console.WriteLine(headerName);
-            }
-            Console.WriteLine("===================================================================================================");
+            GIVEN_HtmlDocument (Page.TableWithThNodes);
+            WHEN_creating_tableProcessor("//table");
+            THEN_tableProcessor_is_ready ();
+            THEN_there_are_N_columns (2);
+            THEN_column_number_N_is (1, "Col1");
+            THEN_column_number_N_is (2, "Col2");
+            THEN_there_are_N_rows (1);
         }
 
         [Test]
         public void HeadersFromFirstRow()
         {
-            var doc = GIVEN_HtmlDocument (Page.TableWithTdNodesAsHeaders);
-            var tableProcessor = new TableProcessor(doc.DocumentNode.SelectNodes("//table").First());
-            Assert.AreEqual(true, tableProcessor.Ready);
-            Assert.AreEqual (2, tableProcessor.ColumnHeaders.Count ());
-            Assert.AreEqual (2, tableProcessor.ColumnHeaderNames.Count ());
-            Assert.AreEqual ("row1col1", tableProcessor.ColumnHeaderNames.First ());
-            Assert.AreEqual ("row1col2", tableProcessor.ColumnHeaderNames.Last ());
-            Assert.AreEqual(1, tableProcessor.Rows.Count());
-            
-            Console.WriteLine("======================================= HeadersFromFirstRow =======================================");
-            foreach(var headerName in tableProcessor.ColumnHeaderNames) {
-                Console.WriteLine(headerName);
-            }
-            Console.WriteLine("===================================================================================================");
+            GIVEN_HtmlDocument (Page.TableWithTdNodesAsHeaders);
+            WHEN_creating_tableProcessor("//table");
+            THEN_tableProcessor_is_ready ();
+            THEN_there_are_N_columns (2);
+            THEN_column_number_N_is (1, "row1col1");
+            THEN_column_number_N_is (2, "row1col2");
+            THEN_there_are_N_rows (1);
         }
 
         [Test]
         public void GeneratedHeaders()
         {
-            var doc = GIVEN_HtmlDocument (Page.TableWithoutHeaders);
-            var tableProcessor = new TableProcessor(doc.DocumentNode.SelectNodes("//table").First(), false) { RowItemExpression = "." };
-            Assert.AreEqual(true, tableProcessor.Ready);
-            Assert.AreEqual (2, tableProcessor.ColumnHeaderNames.Count ());
-            Assert.AreEqual("1", tableProcessor.ColumnHeaderNames.First());
-            Assert.AreEqual("2", tableProcessor.ColumnHeaderNames.Last());
-            Assert.AreEqual(1, tableProcessor.Rows.Count());
-            
-            Console.WriteLine("======================================= GeneratedHeaders =======================================");
-            foreach(var headerName in tableProcessor.ColumnHeaderNames) {
-                Console.WriteLine(headerName);
-            }
-            Console.WriteLine("===================================================================================================");
+            GIVEN_HtmlDocument (Page.TableWithoutHeaders);
+            WHEN_creating_tableProcessor ("//table", false, string.Empty, ".");
+            THEN_tableProcessor_is_ready ();
+            THEN_there_are_N_columns (2);
+            THEN_column_number_N_is (1, "1");
+            THEN_column_number_N_is (2, "2");
+            THEN_there_are_N_rows (1);
         }
         
         [Test]
         public void HeadersNumberFromColumnsNumber()
         {
-            var doc = GIVEN_HtmlDocument (Page.TableWithoutHeadersAndWithVariantNumberOfColumns);
-            // var tableProcessor = new TableProcessor(doc.DocumentNode.SelectNodes("//table").First()) { NoColumnHeaders = true, RowItemExpression = "." };
-            var tableProcessor = new TableProcessor(doc.DocumentNode.SelectNodes("//table").First(), false) { RowItemExpression = "./td" };
-            Assert.AreEqual(true, tableProcessor.Ready);
-            Assert.AreEqual (4, tableProcessor.ColumnHeaderNames.Count ());
-            Assert.AreEqual("1", tableProcessor.ColumnHeaderNames.First());
-            Assert.AreEqual("2", tableProcessor.ColumnHeaderNames.Skip(1).First());
-            Assert.AreEqual("3", tableProcessor.ColumnHeaderNames.Skip(2).First());
-            Assert.AreEqual("4", tableProcessor.ColumnHeaderNames.Last());
-            Assert.AreEqual(4, tableProcessor.Rows.Count());
-            
-            Console.WriteLine("======================================= HeadersNumberFromColumnsNumber =======================================");
-            foreach(var headerName in tableProcessor.ColumnHeaderNames) {
-                Console.WriteLine(headerName);
-            }
-            Console.WriteLine("===================================================================================================");
+            GIVEN_HtmlDocument (Page.TableWithoutHeadersAndWithVariantNumberOfColumns);
+            WHEN_creating_tableProcessor ("//table", false, string.Empty, "./td");
+            THEN_tableProcessor_is_ready ();
+            THEN_there_are_N_columns (4);
+            THEN_column_number_N_is (1, "1");
+            THEN_column_number_N_is (2, "2");
+            THEN_column_number_N_is (3, "3");
+            THEN_column_number_N_is (4, "4");
+            THEN_there_are_N_rows (4);
         }
         
-        HtmlDocument GIVEN_HtmlDocument(string htmlDocument)
+        void GIVEN_HtmlDocument(string htmlDocument)
         {
-            var document = new HtmlDocument ();
-            document.LoadHtml (htmlDocument);
-            return document;
+            _document = new HtmlDocument ();
+            _document.LoadHtml (htmlDocument);
+        }
+
+        void WHEN_creating_tableProcessor(string tableExpression, bool useFirstRowAsHeaders, string columnExpression, string rowItemExpression)
+        {
+            _tableProcessor = new TableProcessor(_document.DocumentNode.SelectNodes(tableExpression).First(), useFirstRowAsHeaders) { ColumnHeaderExpression = columnExpression, RowItemExpression = rowItemExpression };
+        }
+
+        void WHEN_creating_tableProcessor(string tableExpression)
+        {
+            _tableProcessor = new TableProcessor(_document.DocumentNode.SelectNodes(tableExpression).First());
+        }
+
+        void THEN_tableProcessor_is_ready()
+        {
+            Xunit.Assert.Equal(true, _tableProcessor.Ready);
+        }
+
+        void THEN_there_are_N_columns(int columnsNumber)
+        {
+            Xunit.Assert.Equal(columnsNumber, _tableProcessor.ColumnHeaders.Count ());
+            Xunit.Assert.Equal(columnsNumber, _tableProcessor.ColumnHeaderNames.Count ());
+        }
+
+        void THEN_column_number_N_is(int columnNumber, string columnName)
+        {
+            Xunit.Assert.Equal(columnName, _tableProcessor.ColumnHeaderNames.Skip(columnNumber - 1).First ());
+        }
+
+        void THEN_there_are_N_rows(int rowsNumber)
+        {
+            Xunit.Assert.Equal(rowsNumber, _tableProcessor.Rows.Count());
         }
     }
 }
-
