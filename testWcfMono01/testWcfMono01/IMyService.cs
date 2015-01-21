912a527175b33fdee8dad27cf74546a1a157a030
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-22 01:46:55 +0300

    new file:   testMvcMonoDevelop57/testMvcMonoDevelop57.sln 	new file:   testMvcMonoDevelop57/testMvcMonoDevelop57/Global.asax 	new file:   testMvcMonoDevelop57/testMvcMonoDevelop57/Global.asax.cs 	new file:   testMvcMonoDevelop57/testMvcMonoDevelop57/Views/Web.config 	new file:   testMvcMonoDevelop57/testMvcMonoDevelop57/Web.config 	new file:   testMvcMonoDevelop57/testMvcMonoDevelop57/testMvcMonoDevelop57.csproj 	new file:   testWcfMono01/testWcfMono01.sln 	new file:   testWcfMono01/testWcfMono01/IMyService.cs 	new file:   testWcfMono01/testWcfMono01/MyService.cs 	new file:   testWcfMono01/testWcfMono01/Program.cs 	new file:   testWcfMono01/testWcfMono01/Properties/AssemblyInfo.cs 	new file:   testWcfMono01/testWcfMono01/testWcfMono01.csproj

---
+++ testWcfMono01/testWcfMono01/IMyService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace testWcfMono01
+{
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract, WebGet]
+        String GetData();
+    }
+}
+
