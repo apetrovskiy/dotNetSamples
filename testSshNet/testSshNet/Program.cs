8a50f348dc6e991295566af5830c7c005f7b3eda
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-22 22:16:37 +0300

    .

---
+++ testSshNet/testSshNet/Program.cs
@@ -0,0 +1,57 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 1/22/2015
+ * Time: 6:50 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+namespace testSshSharp
+{
+    using System;
+    using Renci.SshNet;
+    
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string vmId = @"(?m)(?n)(?<=^)\d+(?=\s\s+)";
+            const string vmName = @"(?m)(?n)(?<=^[\d]+\s+)[^\s].*[^\s](?=\s+\[)";
+            const string vmFile = @"(?m)(?n)(?<=.*\s+)\[[^\]]+\].*\.vmx(?=\s\s+\w)";
+            const string vmGuestOs = @"";
+            const string vmVersion = @"";
+            const string vmAnnotation = @"";
+            
+            var connInfo = new ConnectionInfo(
+                               "172.28.1.11",
+                               "root",
+                               new AuthenticationMethod[] {
+                    new PasswordAuthenticationMethod("root", @"=1qwerty"),
+                    new PrivateKeyAuthenticationMethod(
+                        "root",
+                        new [] { new PrivateKeyFile(@"e:\putty\my_esxi.key", "=1qwerty") }),
+                });
+            
+            using (var client = new SshClient(connInfo)) {
+                client.Connect();
+                
+                // this works
+                // Console.WriteLine(client.RunCommand("ls -l").Result);
+                
+                using (var cmd = client.CreateCommand("vim-cmd vmsvc/getallvms")) {
+                    var result = cmd.Execute();
+                    Console.WriteLine("row count = {0}", result.Split('\r').Length);
+                    Console.WriteLine(cmd.CommandText);
+                    Console.WriteLine("Return value = {0}", cmd.ExitStatus);
+                    Console.WriteLine(result);
+                }
+                
+               client.Disconnect();
+            }
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
---
+++ testStringSplitter/testStringSplitter/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: alexa_000
+ * Date: 1/22/2015
+ * Time: 8:40 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace testStringSplitter
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            var data = @"aaa
+            bbb
+            ccc
+            ddd
+            eee
+            
+            fff
+            ggg";
+            var array = data.Split('\r');
+            Console.WriteLine(array.Length);
+            
+            Console.Write("Press any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
