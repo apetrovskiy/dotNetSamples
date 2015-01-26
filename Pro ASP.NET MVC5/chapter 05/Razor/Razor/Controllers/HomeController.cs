f761b774355532a76b10c994a464f657b64d0a01
apetrovskiy <apetrovskiy@hotmail.com>
2015-01-26 22:55:12 +0300

    .

---
+++ Pro ASP.NET MVC5/chapter 05/Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        Product _product = new Product
+        {
+            ProductId = 1,
+            Name = "Kayak",
+            Description = "A boat for one person",
+            Category = "Watersports",
+            Price = 275M
+        };
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(_product);
+        }
+    }
+}
