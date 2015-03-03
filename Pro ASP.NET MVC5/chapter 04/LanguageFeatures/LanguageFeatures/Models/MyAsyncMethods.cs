d636c5decb74fd424947c632e277f301b434c200
apetrovskiy <apetrovskiy@hotmail.com>
2015-03-04 00:08:11 +0300

    .

---
+++ Pro ASP.NET MVC5/chapter 04/LanguageFeatures/LanguageFeatures/Models/MyAsyncMethods.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace LanguageFeatures.Models
+{
+    public class MyAsyncMethods
+    {
+        /*
+        public static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpTask = client.GetAsync("http://apress.com");
+            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
+            {
+                return antecedent.Result.Content.Headers.ContentLength;
+            });
+        }
+        */
+
+        public async static Task<long?> GetPageLength()
+        {
+            HttpClient client = new HttpClient();
+            var httpMessage = await client.GetAsync("http://apress.com");
+            return httpMessage.Content.Headers.ContentLength;
+        }
+    }
+}
