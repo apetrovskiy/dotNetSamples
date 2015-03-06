b2359c56212183e41bac6d26e1b952b2ff5650c1
apetrovskiy <apetrovskiy@hotmail.com>
2015-03-07 00:15:49 +0300

    .

--- testHtmlLetterParser/sqlaLetterParser/Program.cs
+++ testHtmlLetterParser/sqlaLetterParser/Program.cs
@@ -19,7 +19,8 @@ namespace sqlaLetterParser
             // doc.Load(@"../../../letters/sqla_2008R2.htm");
             // doc.Load(@"../../../letters/sqla_empty_2008R2.htm");
             // doc.Load(@"../../../letters/1.1.1.111/sqla_2012.htm");
-            doc.Load(@"../../../letters/1.1.1.125/SQLA.2008R2.htm");
+            // doc.Load(@"../../../letters/1.1.1.125/SQLA.2008R2.htm");
+            doc.Load(@"../../../letters/1.1.1.183/sqla_2012.htm");
             
             var tableProcessor = new TableProcessor(doc.DocumentNode.SelectNodes("//table[@id='ChangesTable']").First()) {
                 ColumnHeaderExpression = "./text()",
@@ -41,6 +42,9 @@ namespace sqlaLetterParser
             
             var isTableRecorded = tableProcessor.Exists("Added", "Table", @"Databases\test01\Tables\dbo.ddd", "localhost", @"spanew\suite_admin");
             var isTableContentRecorded = tableProcessor.Exists("Added", "Data Row", @"Databases\test01\Tables\dbo.bbb\Data Row", "localhost", @"spanew\suite_admin");
+
+            Console.WriteLine(isTableRecorded);
+            Console.WriteLine(isTableContentRecorded);
             
             Console.ReadKey ();
         }
