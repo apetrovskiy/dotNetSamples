e6e00e27e35f637c8667efaa3df86fcbedc7abf5
apetrovskiy <apetrovskiy@hotmail.com>
2015-08-03 05:04:25 +0300

    Pro ASP.NET MVC 5 chapter 07+

--- Pro ASP.NET MVC5/07/SportsStore/SportsStore.UnitTests/UnitTest1.cs
+++ Pro ASP.NET MVC5/07/SportsStore/SportsStore.UnitTests/UnitTest1.cs
@@ -32,8 +32,7 @@
             // var controller = GetProductController();
 
             // Act
-            // var result = (IEnumerable<Product>)controller.List(2).Model;
-            var result = (ProductsListViewModel)_controller.List(2).Model;
+            var result = (ProductsListViewModel)_controller.List(null, 2).Model;
 
             // Assert
             // var prodArray = result.ToArray();
@@ -73,22 +72,9 @@
         public void Can_Send_Pagination_View_Model()
         {
             // Arrange
-            /*
-            var mock = new Mock<IProductRepository>();
-            mock.Setup(m => m.Products).Returns(new Product[] {
-                new Product { ProductId = 1, Name = "P1" },
-                new Product { ProductId = 2, Name = "P2" },
-                new Product { ProductId = 3, Name = "P3" },
-                new Product { ProductId = 4, Name = "P4" },
-                new Product { ProductId = 5, Name = "P5" }
-            });
-            var controller = new ProductController(mock.Object);
-            controller.PageSize = 3;
-            */
-            // var controller = GetProductController();
 
             // Act
-            var result = (ProductsListViewModel)_controller.List(2).Model;
+            var result = (ProductsListViewModel)_controller.List(null, 2).Model;
 
             // Assert
             var pageInfo = result.PagingInfo;
@@ -96,6 +82,30 @@
             Assert.AreEqual(pageInfo.ItemsPerPage, 3);
             Assert.AreEqual(pageInfo.TotalItems, 5);
             Assert.AreEqual(pageInfo.TotalPages, 2);
+        }
+
+        [TestMethod]
+        public void Can_Filter_Products()
+        {
+            // Arrange
+            var mock = new Mock<IProductRepository>();
+            mock.Setup(m => m.Products).Returns(new Product[] { 
+                new Product { ProductId = 1, Name = "P1", Category = "Cat1" },
+                new Product { ProductId = 2, Name = "P2", Category = "Cat2" },
+                new Product { ProductId = 3, Name = "P3", Category = "Cat1" },
+                new Product { ProductId = 4, Name = "P4", Category = "Cat2" },
+                new Product { ProductId = 5, Name = "P5", Category = "Cat3" }
+            });
+            var controller = new ProductController(mock.Object);
+            controller.PageSize = 3;
+
+            // Act
+            var result = ((ProductsListViewModel)controller.List("Cat2", 1).Model).Products.ToArray();
+
+            // Assert
+            Assert.AreEqual(result.Length, 2);
+            Assert.IsTrue(result[0].Name == "P2" && result[0].Category == "Cat2");
+            Assert.IsTrue(result[1].Name == "P4" && result[1].Category == "Cat2");
         }
 
         static ProductController GetProductController()
--- Pro ASP.NET MVC5/07/SportsStore/SportsStore.WebUI/Models/ProductsListViewModel.cs
+++ Pro ASP.NET MVC5/07/SportsStore/SportsStore.WebUI/Models/ProductsListViewModel.cs
@@ -1,14 +1,15 @@
 ﻿namespace SportsStore.WebUI.Models
 {
     using SportsStore.Domain.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
 
     public class ProductsListViewModel
     {
         public IEnumerable<Product> Products { get; set; }
         public PagingInfo PagingInfo { get; set; }
+        public string CurrentCategory { get; set; }
     }
 }
