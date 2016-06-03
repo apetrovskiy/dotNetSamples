e4501fe5c1336dc366eb138ad7f04005701754cc
apetrovskiy <uiautomation@hotmail.com>
2016-06-04 00:44:20 +0300

    refactoring to support several web frameworks

---
+++ JdiSampleSite/JdiSampleSite.Common.Library/Modules/NewUserModule.cs
@@ -0,0 +1,81 @@
+﻿namespace JdiSampleSite.Bootstrap.Library.Modules
+{
+    using System;
+    using System.Dynamic;
+    using Nancy;
+    using Nancy.Responses.Negotiation;
+    using System.Linq;
+    using Common.Library;
+    using Common.Library.Data;
+    using Common.Library.Models;
+    using Common.Library.Models.Abstract;
+    using Nancy.ModelBinding;
+    // using Settings;
+
+    public class NewUserModule : NancyModule
+    {
+        // public NewUserModule() : base(Constants.BootstrapRootUrl + Constants.Users)
+        public NewUserModule()
+        // public NewUserModule() : base(BootstrapLibSettings.Path.FrameworkRoot + Constants.Users)
+        {
+            // Post["/"] = _ => CreateNewOrUpdateExistingUser(this.Bind<User>());
+            Post["/{RootPath}" + Constants.Users + "/"] = parameters => CreateNewOrUpdateExistingUser(parameters.RootPath, this.Bind<User>());
+            // this works
+            // Put["/"] = _ => CreateNewOrUpdateExistingUser(this.Bind<User>());
+            Put["/{RootPath}" + Constants.Users + "/"] = parameters => CreateNewOrUpdateExistingUser(parameters.RootPath, this.Bind<User>());
+            // Post[Constants.UserPage] = _ => CreateNewOrUpdateExistingUser(this.Bind<User>());
+            // this works
+            // Put[Constants.UserPage] = _ => CreateNewOrUpdateExistingUser(this.Bind<User>());
+            // Put[Constants.User] = _ => CreateNewOrUpdateExistingUser(this.Bind<User>());
+            Put["/{RootPath}" + Constants.Users + Constants.User] = parameters => CreateNewOrUpdateExistingUser(parameters.RootPath, this.Bind<User>());
+            // Put[Constants.User] = _ => 
+            // Get[Constants.UserPage] = _ => View[Constants.ViewNameUser, new User()];
+            Get["/{RootPath}" + Constants.Users + Constants.UserPage] = parameters => View[parameters.RootPath + "/" + Constants.ViewNameUser, new User()];
+        }
+
+        // Negotiator CreateNewOrUpdateExistingUser(IUser userInfo)
+        Negotiator CreateNewOrUpdateExistingUser(string rootPath, IUser userInfo)
+        {
+            dynamic data = new ExpandoObject();
+            data.Users = UsersCollection.Users;
+            var cookies = Context.NegotiationContext.Cookies;
+
+            if (UsersCollection.Users.All(usr => usr.Id != userInfo.Id))
+            {
+                UsersCollection.AddUser(userInfo);
+                // return Negotiate.WithView(Constants.ViewNameUsers).WithFullNegotiation().WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.TemporaryRedirect).WithHeader("Location", Constants.BootstrapRootUrl + Constants.Users);
+                // return Negotiate.WithView(Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.TemporaryRedirect).WithHeader("Location", Constants.BootstrapRootUrl + Constants.Users).WithCookies(cookies).WithFullNegotiation();
+
+                // this redirects too many times
+                // return Negotiate.WithView(Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.TemporaryRedirect).WithHeader("Location", Constants.BootstrapRootUrl + Constants.Users).WithCookies(cookies).WithFullNegotiation();
+                // return Negotiate.WithView(Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.Created).WithFullNegotiation();
+                return Negotiate.WithView(rootPath + "/" + Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.Created).WithFullNegotiation();
+            }
+
+            //var user = UsersCollection.Users.First(usr => usr.Id == userInfo.Id) ?? new User();
+            //UsersCollection.AddUser(new User
+            //{
+            //    FirstName = userInfo.FirstName,
+            //    LastName = userInfo.LastName,
+            //    BirthDate = userInfo.BirthDate,
+            //    City = userInfo.City,
+            //    Email = userInfo.Email
+            //});
+
+            // UsersCollection.Users.Where(usr => usr.Id == userInfo.Id).ToList().ForEach(usr => usr = userInfo);
+            var existingUser = UsersCollection.Users.First(usr => usr.Id == userInfo.Id);
+            UsersCollection.Users.RemoveAll(usr => usr.Id == userInfo.Id);
+            UsersCollection.AddUser(userInfo);
+
+            // return Negotiate.WithStatusCode(HttpStatusCode.Created).WithView("users");
+            //return View["users"];
+            // return Negotiate.WithView(Constants.ViewNameUsers).WithFullNegotiation().WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.TemporaryRedirect).WithHeader("Location", Constants.BootstrapRootUrl + Constants.Users);
+
+            // this redirects too many times
+            // return Negotiate.WithView(Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.TemporaryRedirect).WithHeader("Location", Constants.BootstrapRootUrl + Constants.Users).WithCookies(cookies).WithFullNegotiation();
+            // return Negotiate.WithView(Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.OK).WithFullNegotiation();
+            return Negotiate.WithView(rootPath + "/" + Constants.ViewNameUsers).WithModel((ExpandoObject)data).WithStatusCode(HttpStatusCode.OK).WithFullNegotiation();
+            // return Negotiate.WithStatusCode(HttpStatusCode.Created).WithView("users.html");
+        }
+    }
+}
---
+++ JdiSampleSite/JdiSampleSite.Common.Library/Modules/UsersModule.cs
@@ -0,0 +1,64 @@
+﻿namespace JdiSampleSite.Bootstrap.Library.Modules
+{
+    using System;
+    using System.Dynamic;
+    using System.Linq;
+    using Common.Library;
+    using Common.Library.Data;
+    using Common.Library.Models;
+    using Common.Library.Models.Abstract;
+    using Nancy;
+    using Nancy.Responses.Negotiation;
+    // using Settings;
+
+    public class UsersModule : NancyModule
+    {
+        // public UsersModule() : base(string.Format(@"{0}|{1}", Constants.BootstrapRootUrl, Constants.FoundationRootUrl) + Constants.Users)
+        // public UsersModule() : base(BootstrapLibSettings.Path.FrameworkRoot + Constants.Users)
+        public UsersModule()
+        {
+            // Get["/"] = _ => DisplayUsers();
+            Get["/{RootPath}" + Constants.Users + "/"] = parameters => DisplayUsers(parameters.RootPath);
+            // Get[Constants.User] = parameters => GetUser(parameters.id);
+            Get["/{RootPath}" + Constants.Users + "/" + Constants.User] = parameters => GetUser(parameters.RootPath, parameters.id);
+            // Delete[Constants.User] = parameters => RemoveUser(parameters.id);
+            Delete["/{RootPath}" + Constants.Users + "/" + Constants.User] = parameters => RemoveUser(parameters.RootPath, parameters.id);
+        }
+
+        // Negotiator DisplayUsers()
+        Negotiator DisplayUsers(string rootPath)
+        {
+            // return Negotiate.WithView(Constants.ViewNameUsers).WithModel(GetUsers()).WithStatusCode(HttpStatusCode.OK).WithFullNegotiation();
+            return Negotiate.WithView(rootPath + "/" + Constants.ViewNameUsers).WithModel(GetUsers()).WithStatusCode(HttpStatusCode.OK).WithFullNegotiation();
+        }
+
+        // Negotiator GetUser(Guid id)
+        Negotiator GetUser(string rootPath, Guid id)
+        {
+            var user = GetUserById(id);
+            // return Negotiate.WithView(Constants.ViewNameUser).WithModel((User)user).WithStatusCode(HttpStatusCode.OK).WithFullNegotiation();
+            return Negotiate.WithView(rootPath + "/" + Constants.ViewNameUser).WithModel((User)user).WithStatusCode(HttpStatusCode.OK).WithFullNegotiation();
+        }
+
+        // Negotiator RemoveUser(Guid id)
+        Negotiator RemoveUser(string rootPath, Guid id)
+        {
+            var user = GetUserById(id);
+            UsersCollection.Users.RemoveAll(usr => usr.Id == user.Id);
+            // return Negotiate.WithView(Constants.ViewNameUsers).WithModel(GetUsers()).WithStatusCode(HttpStatusCode.NoContent).WithFullNegotiation();
+            return Negotiate.WithView(rootPath + "/" + Constants.ViewNameUsers).WithModel(GetUsers()).WithStatusCode(HttpStatusCode.NoContent).WithFullNegotiation();
+        }
+
+        IUser GetUserById(Guid id)
+        {
+            return UsersCollection.Users.FirstOrDefault(usr => usr.Id == id) ?? new User();
+        }
+
+        ExpandoObject GetUsers()
+        {
+            dynamic data = new ExpandoObject();
+            data.Users = UsersCollection.Users;
+            return data;
+        }
+    }
+}
