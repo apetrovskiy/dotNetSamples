66802ebcb66ddb19637aa3503d8df4789332b81a
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2016-06-03 20:18:48 +0300

    more functionality to the analyzer; some changes to the example site

---
+++ JdiCodeGenerator/JdiCodeGenerator.Core/ObjectModel/Abstract/JdiElementTypes.cs
@@ -0,0 +1,17 @@
+﻿namespace JdiCodeGenerator.Core.ObjectModel.Abstract
+{
+    public enum JdiElementTypes
+    {
+        Element,
+        Button,
+        CheckBox,
+        DatePicker,
+        FileInput,
+        Image,
+        Label,
+        Link,
+        Text,
+        TextArea,
+        TextField
+    }
+}
---
+++ JdiCodeGenerator/JdiCodeGenerator.Core/ObjectModel/Plugins/General.cs
@@ -0,0 +1,30 @@
+﻿namespace JdiCodeGenerator.Core.ObjectModel.Plugins
+{
+    using System;
+    using System.Linq;
+    using Abstract;
+
+    public class General // : IFrameworkAlingmentAnalysisPlugin
+    {
+        // public HtmlElementTypes Analyze(HtmlNode node)
+        public HtmlElementTypes Analyze(string originalName)
+        {
+            var result = HtmlElementTypes.Unknown;
+
+            Enum.GetValues(typeof(HtmlElementTypes))
+                .Cast<HtmlElementTypes>()
+                .ToList()
+                .ForEach(htmlElementType =>
+                {
+                    // if (null != node.OriginalName && string.CompareOrdinal(htmlElementType.ToString(), node.OriginalName) != 0)
+                    if (!string.IsNullOrEmpty(originalName) && string.CompareOrdinal(htmlElementType.ToString().ToLower(), originalName) == 0)
+                    {
+                        result = htmlElementType;
+                        return;
+                    }
+                });
+
+            return result;
+        }
+    }
+}
