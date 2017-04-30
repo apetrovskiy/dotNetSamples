d0a64e91ee13d2e4286e9c95b89d4ca53a083592
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2017-04-30 21:15:54 +0300

    .

---
+++ AutoMapperTest007_5_to_6/AutoMapperTest007_5_to_6/Class1.cs
@@ -0,0 +1,9 @@
+﻿namespace AutoMapperTest003
+{
+    class Class1
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
