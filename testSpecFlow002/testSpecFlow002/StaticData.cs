2bc44b47ed21c31b835d60ecaac6a864cf72057a
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2017-05-31 12:48:39 +0300

    .

---
+++ testSpecFlow002/testSpecFlow002/StaticData.cs
@@ -0,0 +1,8 @@
+﻿namespace testSpecFlow002
+{
+    public class StaticData
+    {
+        public static bool Flag { get; set; }
+        public static bool InitialValue { get; set; }
+    }
+}
---
+++ testSpecFlow002/testSpecFlow002/Steps/CommonSteps.cs
@@ -0,0 +1,16 @@
+﻿namespace testSpecFlow002.Steps
+{
+    using TechTalk.SpecFlow;
+
+    [Binding]
+
+    public class CommonSteps
+    {
+        [Given(@"I set flag=(.*)")]
+        [Then(@"I set flag=(.*)")]
+        public void GivenISetFlag(bool value)
+        {
+            StaticData.Flag = value;
+        }
+    }
+}
