e396ecf0624987b9e2761a1fa2df71c731564a7c
Alexander Petrovskiy <uiautomation@hotmail.com>
2018-05-03 19:54:55 +0300

    .

---
+++ ASP.NET MVC Core 2/WorkingWithVisualStudio/WorkingWithVisualStudio/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿namespace WorkingWithVisualStudio.Controllers
+{
+	using System.Linq;
+
+	using Microsoft.AspNetCore.Mvc;
+
+	using WorkingWithVisualStudio.Models;
+
+	public class HomeController : Controller
+	{
+		// GET
+		// public IActionResult Index() => View(SimpleRepository.SharedRepository.Products);
+		public IActionResult Index() => View(SimpleRepository.SharedRepository.Products.Where(p => p.Price < 50));
+	}
+}
---
+++ ASP.NET MVC Core 2/WorkingWithVisualStudio/WorkingWithVisualStudio/Models/SimpleRepository.cs
@@ -0,0 +1,32 @@
+﻿namespace WorkingWithVisualStudio.Models
+{
+	using System.Collections.Generic;
+
+	public class SimpleRepository
+	{
+		private static SimpleRepository sharedRepository = new SimpleRepository();
+		private Dictionary<string, Product> products = new Dictionary<string, Product>();
+
+		public static SimpleRepository SharedRepository => sharedRepository;
+
+		public SimpleRepository()
+		{
+			var initialItems = new[]
+				                   {
+					                   new Product { Name = "Kayak", Price = 275M },
+									   new Product { Name = "Lifejacket", Price = 48.95M },
+									   new Product { Name = "Soccer ball", Price = 19.50M },
+									   new Product { Name = "Corner flag", Price = 34.95M }
+				                   };
+			foreach (var p in initialItems)
+			{
+				AddProduct(p);
+			}
+			this.products.Add("Error", null);
+		}
+
+		public IEnumerable<Product> Products => this.products.Values;
+
+		public void AddProduct(Product p) => this.products.Add(p.Name, p);
+	}
+}
