28d4a0863b1fd20bfb000ca9010fd82685f6d918
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2020-08-10 00:58:57 +0300

    the third test

--- unit-testing-with-fsharp/MathService/MyMath.fs
+++ unit-testing-with-fsharp/MathService/MyMath.fs
@@ -1,7 +1,9 @@
 namespace MathService
 
 module MyMath =
+    let square x = x * x
     let private isOdd x = x % 2 <> 0
     let squaresOfOdds xs =
         xs
         |> Seq.filter isOdd
+        |> Seq.map square
