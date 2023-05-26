3b811ba3eec609dda42d4a78405b25ed214ba83b
Alexander Petrovskiy <apetrovskiy@hotmail.com>
2023-05-26 06:16:43 +0300

    the initial version of C# and F# webapi (#116)

---
+++ aspapi_f/Program.fs
@@ -0,0 +1,42 @@
+namespace aspapi_f
+#nowarn "20"
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.HttpsPolicy
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.DependencyInjection
+open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+
+module Program =
+    let exitCode = 0
+
+    [<EntryPoint>]
+    let main args =
+
+        let builder = WebApplication.CreateBuilder(args)
+
+        builder.Services.AddControllers()
+        builder.Services.AddEndpointsApiExplorer()
+        // builder.Services.AddSwaggerGen()
+
+        let app = builder.Build()
+
+        // if app.Environment.IsDevelopment() then
+        //     app.UseSwagger()
+        //     app.UseSwaggerUI()
+
+        app.UseHttpsRedirection()
+
+        app.UseAuthorization()
+        app.MapControllers()
+
+        app.Run()
+
+        exitCode
---
+++ aspapi_f/WeatherForecast.fs
@@ -0,0 +1,11 @@
+namespace aspapi_f
+
+open System
+
+type WeatherForecast =
+    { Date: DateTime
+      TemperatureC: int
+      Summary: string }
+
+    member this.TemperatureF =
+        32.0 + (float this.TemperatureC / 0.5556)
