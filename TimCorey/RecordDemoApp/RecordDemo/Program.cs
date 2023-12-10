da085f05eb740a653cd690b13338b695b883f179
Aleksandr Petrovskiy <uiautomation@hotmail.com>
2023-12-10 16:08:38 +0300

    further changes

--- TimCorey/RecordDemoApp/RecordDemo/Program.cs
+++ TimCorey/RecordDemoApp/RecordDemo/Program.cs
@@ -1,5 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Record1 r1a = new("Tim", "Corey");
 Record1 r1b = new("Tim", "Corey");
 Record1 r1c = new("Sue", "Storm");
@@ -10,6 +9,13 @@ Class1 c1c = new("Sue", "Storm");
 
 Console.WriteLine("Record Type:");
 Console.WriteLine($"To String: {r1a}");
+Console.WriteLine($"Are the two objects equal: {Equals(r1a, r1b)}");
+Console.WriteLine($"Are the two objects reference equal: {ReferenceEquals(r1a, r1b)}");
+Console.WriteLine($"Are the two objects ==: {r1a == r1b}");
+Console.WriteLine($"Are the two objects !=: {r1a != r1c}");
+Console.WriteLine($"Hash code of object A: {r1a.GetHashCode()}");
+Console.WriteLine($"Hash code of object B: {r1b.GetHashCode()}");
+Console.WriteLine($"Hash code of object C: {r1c.GetHashCode()}");
 
 Console.WriteLine();
 Console.WriteLine("***************************");
@@ -17,6 +23,14 @@ Console.WriteLine();
 
 Console.WriteLine("Class Type:");
 Console.WriteLine($"To String: {c1a}");
+Console.WriteLine($"Are the two objects equal: {Equals(c1a, c1b)}");
+Console.WriteLine($"Are the two objects reference equal: {ReferenceEquals(c1a, c1b)}");
+Console.WriteLine($"Are the two objects ==: {c1a == c1b}");
+Console.WriteLine($"Are the two objects !=: {c1a != c1c}");
+Console.WriteLine($"Hash code of object A: {c1a.GetHashCode()}");
+Console.WriteLine($"Hash code of object B: {c1b.GetHashCode()}");
+Console.WriteLine($"Hash code of object C: {c1c.GetHashCode()}");
+
 
 // a Record is just a fancy class
 // Immutable - The values cannot be changed
