9656bfb7a88a8b7f18449f160b087b8f60946f8c
Alexander Petrovskiy <uiautomation@hotmail.com>
2024-12-25 01:19:33 +0300

    solution has built

---
+++ EFSamplesNet8/EFSampleNet8.Core/Models/Sqlite/SqliteContext.cs
@@ -0,0 +1,17 @@
+﻿// <copyright file="SqliteContext.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace EFSampleNet8.Core.Models.Sqlite;
+
+using Microsoft.EntityFrameworkCore;
+
+public class SqliteContext : DbContext
+{
+    public SqliteContext(DbContextOptions<SqliteContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Recipe> Recipes { get; set; }
+}
